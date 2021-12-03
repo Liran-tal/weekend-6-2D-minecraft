cf0e5e702f6d9cfaf9734a9653fc5d3e2e8cd61a
Liran-tal <taliran44@gmail.com>
2021-12-03 12:21:40 +0200

    'םרל םמ אםםךד

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,25 @@
 "use strict";
 
 const gameBoard = document.querySelector("#game-board");
-
-
+const toolBox = document.querySelector("#tool-box");
+const inventory = document.querySelector('.inventory');
 const INVENTORY_CAPACITY = 8;
-const classesInventory = Array(INVENTORY_CAPACITY);
+
+// const gameElements = {
+// 	const gameBoard = document.querySelector("#game-board");
+// 	const inventory = document.querySelector('.inventory');
+// 	const INVENTORY_CAPACITY = 8;
+// 	const materialArr = [
+// 		null,
+// 		"tree",
+// 		"leaves",
+// 		"rock",
+// 		"ground",
+// 		"grass",
+// 		"cloud"
+// 	]
+	
+// }
 
 const materialArr = [
 	null,
@@ -42,32 +57,39 @@ const materialArr = [
 		[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
 	];
 	// return gameBoardMatrix;
+	// }
+	
+	// *** Build game board ***
+	// function setGameBoard(columns, rows) {
+		// gameBoardMatrix = createMatrix(columns, rows);
+		// runs on each row
+		gameBoardMatrix.forEach((row, yIndex) => {
+			// runs on each column
+			row.forEach((column, xIndex) => {
+				// save current position id
+				const currentPositionId = gameBoardMatrix[yIndex][xIndex];
+				// create a block
+				const block = document.createElement("div");
+				// add style by id
+				if (currentPositionId !== 0) {
+					block.classList.add(materialArr[currentPositionId]);
+				}
+				// add to html
+				gameBoard.appendChild(block);
+			})
+		});
 // }
+		
+function toolHandler(target) {
+	const tool = target.classList[1];
+}
+function markSelected(elm) {
+	elm.classList.contains('in-use') ? 
+}
 
-// *** Build game board ***
-// function setGameBoard(columns, rows) {
-	// gameBoardMatrix = createMatrix(columns, rows);
-	// runs on each row
-	gameBoardMatrix.forEach((row, yIndex) => {
-		// runs on each column
-		row.forEach((column, xIndex) => {
-			// save current position id
-			const currentPositionId = gameBoardMatrix[yIndex][xIndex];
-			// create a block
-			const block = document.createElement("div");
-			// add style by id
-			if (currentPositionId !== 0) {
-				block.classList.add(materialArr[currentPositionId]);
-			}
-			// add to html
-			gameBoard.appendChild(block);
-		})
-	});
-// }
 
-// Build inventory
+		// Build inventory
 // function createInventory() {
-	const inventory = document.querySelector('.inventory')
 	const num_columns = (INVENTORY_CAPACITY > 15) ? 3 : 2;
 	const num_rows = Math.floor(INVENTORY_CAPACITY / num_columns);
 	for (let i = 1; i <= num_rows; ++ i){
@@ -77,8 +99,21 @@ const materialArr = [
 			inventory.append(item);
 		}
 	}
+	const materialsInventory = [...inventory.children];
 // }
-
-function addToInventory({target}) {
+	
+function editInventory(event) {
 	
 }
+
+function addToInventory({target}) {
+	const first_free_box = materialsInventory.find((div) => {
+		div.classList[0] === undefined;
+	})
+
+	first_free_box.classList.add(target.classList[0]);
+}
+
+toolBox.addEventListener("click" ({}));
+
+gameBoard.addEventListener("click", )
